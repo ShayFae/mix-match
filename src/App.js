66290432d1662657055b317bb2b20766d238be58
ShayFae <ifrahnoor108@gmail.com>
2022-05-29 23:53:11 -0400

    add shuffle function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 function App() {
+let cards = ['King', 'King', 'Queen', 'Queen', 'Jack', 'Jack']
+
+let parseCards = cards.map(x => x)
+
+// const shuffle = () => {
+//   for(let i = 0; i < cards.length; i++) {
+//     let num = Math.floor(Math.random() * cards.length);
+//   }
+// }
+
   return (
     <div className="App">
-      </header>
+      <div>
+        <p>{parseCards}</p>
+      </div>
+      <button onClick={shuffle(cards)}>Shuffle</button>
     </div>
   );
 }
